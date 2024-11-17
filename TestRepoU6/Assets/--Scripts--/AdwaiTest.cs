836756078dc7967c06c1508429c48cd93bff438b
AdwaiB <bogoluad@msu.edu>
2024-11-17 18:25:50 -0500

    test

---
+++ TestRepoU6/Assets/--Scripts--/AdwaiTest.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class AdwaiTest : MonoBehaviour
+{
+  
+    void Start()
+    {
+         Debug.Log("Hi My Name Is Adwai");
+    }
+    void Update()
+    {
+       
+    }
+}
